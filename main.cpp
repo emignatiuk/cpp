723446766429ada2a1423addcc09c9f4f9b3ebb1
emignatiuk <emigna@st.amu.edu.pl>
2025-03-06 08:25:57 +0100

    Add files via upload
    PROT

--- main.cpp
+++ main.cpp
@@ -1,34 +1,53 @@
+
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <unordered_map>
 
-class ReverseComplementDNA {
+class ProteinTranslation {
 private:
-    std::string sekwencja;
+    std::string rna;
+    std::unordered_map<std::string, char> kodony;
 
 public:
-    ReverseComplementDNA(const std::string& dna) {
-        sekwencja = dna;
-        std::reverse(sekwencja.begin(), sekwencja.end());
-        for (char& nukleotyd : sekwencja) {
-            switch (nukleotyd) {
-                case 'A': nukleotyd = 'T'; break;
-                case 'T': nukleotyd = 'A'; break;
-                case 'C': nukleotyd = 'G'; break;
-                case 'G': nukleotyd = 'C'; break;
-            }
-        }
+    ProteinTranslation(const std::string& sekwencja_RNA) : rna(sekwencja_RNA) {
+        initializeKodony();
     }
 
-    void printReverseComplement() const {
-        std::cout << sekwencja << std::endl;
+    void initializeKodony() {
+        kodony = {
+                {"AUG", 'M'}, {"UUU", 'F'}, {"UUC", 'F'}, {"UUA", 'L'}, {"UUG", 'L'},
+                {"UCU", 'S'}, {"UCC", 'S'}, {"UCA", 'S'}, {"UCG", 'S'},
+                {"UAU", 'Y'}, {"UAC", 'Y'}, {"UAA", '*'}, {"UAG", '*'},
+                {"UGU", 'C'}, {"UGC", 'C'}, {"UGA", '*'}, {"UGG", 'W'},
+                {"CUU", 'L'}, {"CUC", 'L'}, {"CUA", 'L'}, {"CUG", 'L'},
+                {"CCU", 'P'}, {"CCC", 'P'}, {"CCA", 'P'}, {"CCG", 'P'},
+                {"CAU", 'H'}, {"CAC", 'H'}, {"CAA", 'Q'}, {"CAG", 'Q'},
+                {"CGU", 'R'}, {"CGC", 'R'}, {"CGA", 'R'}, {"CGG", 'R'},
+                {"AUU", 'I'}, {"AUC", 'I'}, {"AUA", 'I'}, {"AAG", 'K'}, {"AAA", 'K'},
+                {"ACU", 'T'}, {"ACC", 'T'}, {"ACA", 'T'}, {"ACG", 'T'},
+                {"AGU", 'S'}, {"AGC", 'S'}, {"AGA", 'R'}, {"AGG", 'R'},
+                {"GUU", 'V'}, {"GUC", 'V'}, {"GUA", 'V'}, {"GUG", 'V'},
+                {"GCU", 'A'}, {"GCC", 'A'}, {"GCA", 'A'}, {"GCG", 'A'},
+                {"GAU", 'D'}, {"GAC", 'D'}, {"GAA", 'E'}, {"GAG", 'E'},
+                {"GGU", 'G'}, {"GGC", 'G'}, {"GGA", 'G'}, {"GGG", 'G'}
+        };
+    }
+
+    std::string translate() {
+        std::string białko;
+        for (size_t i = 0; i < rna.length(); i += 3) {
+            std::string codon = rna.substr(i, 3);
+            if (kodony[codon] == '*') break;
+            białko += kodony[codon];
+        }
+        return białko;
     }
 };
 
 int main() {
-    std::string dna;
-    std::cin >> dna;
-    ReverseComplementDNA reverseComplement(dna);
-    reverseComplement.printReverseComplement();
+    std::string rna;
+    std::cin >> rna;
+    ProteinTranslation translator(rna);
+    std::cout << translator.translate() << std::endl;
     return 0;
 }
